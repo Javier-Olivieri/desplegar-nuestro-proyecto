0c10ebe6f0d3e666d4235a87da8c3e9f8cb11e1d
Javier Olivieri <90360071+Javier-Olivieri@users.noreply.github.com>
2022-07-04 10:46:39 -0300

    desafio

---
+++ public/scripts/config.js
@@ -0,0 +1,11 @@
+const PORT = 8080;
+//const ENVIROMENT = "LOCAL";
+const ENVIROMENT = "HEROKU";
+
+
+let BASEURL = '';
+
+if (ENVIROMENT == "LOCAL")
+    BASEURL = `http://localhost:${PORT}/api`;
+else
+    BASEURL = `https://desafio-34-coderhouse.herokuapp.com/api`;
